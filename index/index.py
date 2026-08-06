ad29698b96bca3662a7f383d82b84b130280a2bb
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-08-06 16:10:13 +0530

    Learning Functions

--- index/index.py
+++ index/index.py
@@ -1,10 +1,2 @@
-product1=float(input("Enter the price of product 1: "))
-product2=float(input("Enter the price of product 2: "))
-product3=float(input("Enter the price of product 3: "))
-total = product1 + product2 + product3
-print("The total price of the products is: ", total)
-Average = total / 3
-print("The average price of the products is: ", Average)
-name = input("Enter your name: ")
-print("Hello, ", name, "! Thank you for shopping with us.")
-print(name.find("S"))
+#Function 
+
