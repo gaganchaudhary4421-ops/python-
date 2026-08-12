895e55789ebb12a60d379bf70d5adfc55ed50123
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-12 19:04:47 +0530

    Learning Conditional Statements

--- index/index.py
+++ index/index.py
@@ -1,6 +1,18 @@
 #conditional Statements 
-age = 18
-if age > 18:
-    print("bssds")
+#OOPs
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def is_adult(self):
+        return self.age >= 18
+
+# Create an instance of the Person class
+person = Person("Alice", 20)
+
+# Check if the person is an adult
+if person.is_adult():
+    print(f"{person.name} is an adult.")
 else:
     print("sbs")
