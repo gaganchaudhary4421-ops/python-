851697f80782c7686822f858babacec8f7dd27ea
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-13 15:17:50 +0530

    Practicing Problems

--- index/index.py
+++ index/index.py
@@ -1,11 +1,22 @@
-class car:
-    def __init__(self):
-        self.acc=False
-        self.clutch=False
-        self.brk=False
-    def Start(self):
-        self.clutch = True
-        self.acc = True
-        print("Car Started")
-car1 = car()
-car1.Start()
+class Account:
+    def __init__(self, balance, account_no):
+        self.balance = balance
+        self.account_no = account_no
+
+    def display_account_info(self):
+        print(f"Account Number: {self.account_no}, Balance: {self.balance}")
+    def Debit(self,amount):
+        self.balance -= amount
+        print(f"Debited {amount}. New Balance: {self.balance}")
+         
+    def Credit(self,amount):
+            self.balance += amount
+            print(f"Credited {amount}. New Balance: {self.balance}")
+             
+    def get_balance(self):
+        return self.balance
+    
+Account1 = Account(1000, "123456789")
+Account1.Debit(1000)
+Account1.Credit(500)
+Account1.display_account_info()
