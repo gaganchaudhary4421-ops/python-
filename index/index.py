d00261d4049c72b47413331e449dee3f313665d6
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-13 15:04:32 +0530

    Learning Abstraction

--- index/index.py
+++ index/index.py
@@ -1,15 +1,11 @@
-#class and instance Attributes
-class student:
-    school="ABC_School"
-    name = "Anonymous"#class attribute
-    def __init__(self,name,marks):
-        self.name=name
-        self.marks=marks
-    def Avg(self):
-        sum = 0
-        for i in self.marks:
-            sum +=i
-        print("Average marks of",self.name,"is",sum/len(self.marks))
-s1 = student("Amit",[90,98,37])
-s1.Avg()
-#Learning Static Methods ,decorators
+class car:
+    def __init__(self):
+        self.acc=False
+        self.clutch=False
+        self.brk=False
+    def Start(self):
+        self.clutch = True
+        self.acc = True
+        print("Car Started")
+car1 = car()
+car1.Start()
