ec9033e40bac4425dd68071e7e371bcc6df68cf3
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-13 14:24:28 +0530

    Learning Class and objects Attribute

--- index/index.py
+++ index/index.py
@@ -1,1 +1,9 @@
 #class and instance Attributes
+class student:
+    school="ABC_School"
+    name = "Anonymous"#class attribute
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+s1 = student("Amit",90)
+print(s1.name)
