c0ada6890617ce7477d170190c71a721092f57f2
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-14 09:31:17 +0530

    Learning Private Attribute and Method

--- index/index.py
+++ index/index.py
@@ -1,22 +1,8 @@
-class Account:
-    def __init__(self, balance, account_no):
-        self.balance = balance
-        self.account_no = account_no
-
-    def display_account_info(self):
-        print(f"Account Number: {self.account_no}, Balance: {self.balance}")
-    def Debit(self,amount):
-        self.balance -= amount
-        print(f"Debited {amount}. New Balance: {self.balance}")
-         
-    def Credit(self,amount):
-            self.balance += amount
-            print(f"Credited {amount}. New Balance: {self.balance}")
-             
-    def get_balance(self):
-        return self.balance
-    
-Account1 = Account(1000, "123456789")
-Account1.Debit(1000)
-Account1.Credit(500)
-Account1.display_account_info()
+class person:
+    __name = "Anonymous"
+    def __hello(self):
+        print("Hello person")
+    def welcome(self):
+        self.__hello()
+p1 = person()
+print(p1.welcome())
