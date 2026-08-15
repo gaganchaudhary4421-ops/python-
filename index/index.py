a49107a03d0d3ce98b7f861c9623be36453efcbf
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-15 20:52:27 +0530

    Learning Polymorphism

--- index/index.py
+++ index/index.py
@@ -1,10 +1,1 @@
-class student:
-    def __init__(self,phy,chem,math):
-        self.phy = phy
-        self.chem = chem
-        self.math = math
-    @property
-    def CalculatePercentage(self):
-        return str((self.phy + self.chem + self.math) / 3) + "%"
-stud1=student(98,77,67)
-print(stud1.CalculatePercentage)
+#Polymorphism
