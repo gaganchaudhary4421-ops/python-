cc5832688a98d609baefd4f9313fa0cc5d29cfe0
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-15 20:43:55 +0530

    Learning Decorators

--- index/index.py
+++ index/index.py
@@ -1,17 +1,10 @@
-class car:
-    @staticmethod
-    def start():
-        print("Car Started")
-    @staticmethod
-    def stop():
-        print("Car Stopped")
-class ToyotaCar(car):
-     def __init__(self,name,model):
-         self.name = name
-         self.model = model
-car1 = ToyotaCar("Toyota", "Corolla")
-car2 = ToyotaCar("Toyota", "Camry")
-print(car1.name, car1.model,car1.start())
-print(car2.name, car2.model,car2.stop())
-#multiple inheritance
-#Multi-Level Inheritance
+class student:
+    def __init__(self,phy,chem,math):
+        self.phy = phy
+        self.chem = chem
+        self.math = math
+    @property
+    def CalculatePercentage(self):
+        return str((self.phy + self.chem + self.math) / 3) + "%"
+stud1=student(98,77,67)
+print(stud1.CalculatePercentage)
