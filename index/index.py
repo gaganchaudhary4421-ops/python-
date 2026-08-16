a35c3f8dea8565654682d07a863e2e3142405bc1
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-16 20:47:11 +0530

    Solve practice question

--- index/index.py
+++ index/index.py
@@ -1,15 +1,9 @@
-class Employee:
-    def __init__(self,role,department,salary):
-        self.role=role
-        self.department=department
-        self.salary=salary
-    def showDetails(self):
-        print(f"Role: {self.role}, Department: {self.department}, Salary: {self.salary}")
-class Engineer(Employee):
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-        super().__init__("Engineer", "Engineering", 60000)
-engg1=Engineer("Elon Musk",40)
-engg1.showDetails()
- 
+class Order:
+    def __init__(self,item,price):
+        self.item=item
+        self.price=price
+    def __gt__(self,odr2):
+        return self.price>odr2.price
+odr1=Order("Laptop",1000)
+odr2=Order("Mobile",500)
+print(odr1>odr2)
