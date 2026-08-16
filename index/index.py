b59aae5de799b6922d62752b6da4722ac4931fd4
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-16 12:28:49 +0530

    Learning Functions

--- index/index.py
+++ index/index.py
@@ -1,18 +1,6 @@
-#Polymorphism
-class Complex:
-    def __init__(self,real,img):
-        self.real=real
-        self.img=img
-    def showNumber(self):
-        print(self.real,  "i +", self.img ,"j")
-    def __add__(self,num2):
-        newReal=self.real+num2.real
-        newImg=self.img+num2.img
-        return Complex(newReal,newImg)
-num1 = Complex(1,4)
-num1.showNumber()
-num2 = Complex(4,6)
-num2.showNumber()
-num3 = num1 + num2
-num3.showNumber()
-
+#Average of 3 numbers
+def Calc_Avg(a,b,c):
+    sum = a+b+c
+    avg = sum/3
+    print("Avg of 3 numbers is:", avg)
+Calc_Avg(3,5,7)
