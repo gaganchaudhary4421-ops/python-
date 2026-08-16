2422201eadb63b75b9448fd6b5050de41910b28c
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-16 20:36:14 +0530

    Find Area and Perimeter of Circle

--- index/index.py
+++ index/index.py
@@ -1,6 +1,13 @@
-#Average of 3 numbers
-def Calc_Avg(a,b,c):
-    sum = a+b+c
-    avg = sum/3
-    print("Avg of 3 numbers is:", avg)
-Calc_Avg(3,5,7)#Calculate Average of 3 numbers
+class circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14159 * (self.radius ** 2)
+
+    def Perimeter(self):
+        return 2 * 3.14159 * self.radius
+Area=circle(5).area()
+perimeter=circle(5).Perimeter()
+print("Area of the circle:", Area)
+print("Perimeter of the circle:", perimeter)
