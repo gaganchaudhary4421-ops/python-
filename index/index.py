39669e440f0e975ed3a60be14eb287378ea80188
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-17 12:52:39 +0530

    Solve practice question for finding length of the list

--- index/index.py
+++ index/index.py
@@ -1,6 +1,5 @@
-cities=["New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose"]
-Brand = ["Apple", "Samsung", "Google", "Microsoft", "Amazon", "Facebook", "Tesla", "Nike", "Adidas", "Coca-Cola"]
-def print_length(list):
-    print(len(list))
-print_length(cities)
-print_length(Brand)
+def Converter(usd_val):
+    inr = usd_val * 90
+    print(usd_val, "USD =", inr,"INR")
+Converter(67)
+#learning functions and recursion
