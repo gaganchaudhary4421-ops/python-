37ee30cb9266d4977238bcf6bdeca5156df5a2d4
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-19 20:11:42 +0530

    #Learning Recursion

--- index/index.py
+++ index/index.py
@@ -1,7 +1,6 @@
-#Learning Recursion
-def show(n):
-    if(n==0):
-        return
-    print(n)
-    show(n-1)
-show(50)
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
+print(fact(99))
