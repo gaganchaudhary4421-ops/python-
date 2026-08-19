421ac535af1b7b3238b4b38a9089df7799ea9f57
AKASH CHAUDHARY <akash8057297gmail.com@AKASHs-MacBook-Air-2.local>
2026-08-19 20:18:13 +0530

    #Learning Recursion

--- index/index.py
+++ index/index.py
@@ -1,6 +1,7 @@
-def sum(n):
-    if n == 0:
-        return 0
-    else:
-        return n + sum(n - 1)
-print(sum(5))
+def print_list(list,idx):
+    if(idx == len(list)):
+        return
+        print(list[idx])
+        print_list(list,idx+1)
+cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+print_list(cities, 0)
